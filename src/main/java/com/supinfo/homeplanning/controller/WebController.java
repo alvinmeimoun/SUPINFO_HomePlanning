b4cd50bc20a038314cf8a964152b87acbf528255
Alvin Meimoun <alvin.meimoun@gmail.com>
2016-01-20 16:26:32 +0100

    chore(project): Init project with some pages and base security configuration

---
+++ src/main/java/com/supinfo/homeplanning/controller/WebController.java
@@ -0,0 +1,25 @@
+package com.supinfo.homeplanning.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class WebController {
+
+    @RequestMapping(path = { "/", "/home"}, method = RequestMethod.GET)
+    public ModelAndView home(){
+        return new ModelAndView("home");
+    }
+
+    @RequestMapping(path = "/hello", method = RequestMethod.GET)
+    public ModelAndView hello(){
+        return new ModelAndView("hello");
+    }
+
+    @RequestMapping(path = "/login", method = RequestMethod.GET)
+    public ModelAndView login(){
+        return new ModelAndView("login");
+    }
+}
