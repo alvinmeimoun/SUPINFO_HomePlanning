d7badfa9dbac3df058df7f1987af39ae947f66f2
Alvin Meimoun <alvin.meimoun@gmail.com>
2016-03-26 14:57:01 +0100

    feat(home): Binded messages to home model

--- src/main/java/com/supinfo/homeplanning/service/PublicService.java
+++ src/main/java/com/supinfo/homeplanning/service/PublicService.java
@@ -1,13 +1,33 @@
 package com.supinfo.homeplanning.service;
 
+import com.supinfo.homeplanning.controller.MessageController;
+import com.supinfo.homeplanning.entity.MessageEntity;
 import com.supinfo.homeplanning.model.HomeModel;
+import com.supinfo.homeplanning.repository.MessageRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.stream.Collectors;
 
 @Service
 public class PublicService {
 
+    @Autowired
+    MessageService messageService;
+
+    @Autowired
+    MessageRepository messageRepository;
+
+    @Autowired
+    PlanningService planningService;
+
     public HomeModel generateHomeModel(){
-        return new HomeModel();
+        HomeModel model = new HomeModel();
+
+        model.setMessages(messageRepository.findAll().stream().map(MessageEntity::getMessage)
+                .collect(Collectors.toList()));
+
+        return model;
     }
 
 }
