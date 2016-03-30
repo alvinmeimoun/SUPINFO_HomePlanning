12dbd140092bc30eb615d9ca25a2b3d6c9280172
Alvin Meimoun <alvin.meimoun@gmail.com>
2016-03-30 13:42:47 +0200

    feat(planning): Date parametrable en requete get

--- src/main/java/com/supinfo/homeplanning/controller/PublicController.java
+++ src/main/java/com/supinfo/homeplanning/controller/PublicController.java
@@ -3,9 +3,11 @@ package com.supinfo.homeplanning.controller;
 import com.supinfo.homeplanning.model.HomeModel;
 import com.supinfo.homeplanning.service.PublicService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.time.LocalDate;
@@ -20,12 +22,11 @@ public class PublicController {
     PublicService publicService;
 
     @RequestMapping(path = { "/", "/home"}, method = RequestMethod.GET)
-    public ModelAndView home(){
-        //TODO set today date
-        Date planningDate = Date.from(LocalDate.of(2015, Month.NOVEMBER, 3).atTime(00,00).atZone(ZoneId.systemDefault()).toInstant());
-        //Date planningDate = new Date(System.currentTimeMillis());
+    public ModelAndView home(@RequestParam(name = "date", required = false)
+                                 @DateTimeFormat(pattern="dd-MM-yyyy") Date planningDate){
+        Date selectedDate = planningDate != null ? planningDate : new Date(System.currentTimeMillis());
 
-        HomeModel model = publicService.generateHomeModel(planningDate);
+        HomeModel model = publicService.generateHomeModel(selectedDate);
         return new ModelAndView("home", "homeModel", model);
     }
 
